73f39475cf59bdee97c3f86ec54a5d1a418677cf
dillandatanerd <dillanpatelnz@gmail.com>
2025-08-13 16:49:28 +0200

    added recipe block

--- recipe-book/src/components/RecipeTable.jsx
+++ recipe-book/src/components/RecipeTable.jsx
@@ -1,6 +1,34 @@
+import recipes from "../assets/recipes.json"
+let x=recipes[0]
+
 function RecipeTable() {
-  return (
-    <div>RecipeTable</div>
+    return (
+
+    <>
+    <div className="recipe">
+<div className="imageContainer">
+    <img src={x.image} alt="recipe image"></img>
+</div>
+    <div className="information"> 
+    <h2>{x.name}</h2>
+    <p><span>Calories:</span> {x.calories}</p>
+    <p> <span>Serving: </span>{x.servings}</p>
+</div>
+</div>
+
+
+    </>
   )
 }
 export default RecipeTable
+
+
+
+
+//   {
+//     "id": "9e7f0d61-aa6b-418a-b56a-6418d19e6e1d",
+//     "name": "Pizza",
+//     "calories": 400,
+//     "image": "https://i.imgur.com/eTmWoAN.png",
+//     "servings": 1
+//   },
