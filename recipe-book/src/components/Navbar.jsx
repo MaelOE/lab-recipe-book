76f11d996025a4a7e903fe56a5cf80f51c8f2cb0
Maël Odina Eugène <maodinae@gmail.com>
2025-08-19 19:56:56 +0200

    merging

--- recipe-book/src/components/Navbar.jsx
+++ recipe-book/src/components/Navbar.jsx
@@ -1,15 +1,14 @@
-
 function Navbar() {
   return (
-    <>  
-    
-    <div id="navbar">
-      <h1> French Chef </h1>
-      <img src={"/src/assets/applogo.png"} id="logo"></img>
-
-    </div>
+    <>
+      <div id="navbar">
+        <div className="center">
+          <h1> French Chef </h1>
+        </div>
+        <img src={"/src/assets/applogo.png"} id="logo"></img>
+      </div>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- recipe-book/src/components/Sidebar.jsx
+++ recipe-book/src/components/Sidebar.jsx
@@ -1,17 +1,28 @@
 // import AboutPage from "../Pages/Aboutpage"
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function Sidebar() {
   return (
-
     <>
-    <div id="sidebar">
-      <Link to = "/"> <h3>Dashboard</h3> </Link>
-      <Link to = "/about"> <h3>About</h3> </Link>
-      <Link to = "/addRecipe"> <h3>Add Recipe</h3> </Link>
-    </div>
-</>
-  )
+      <div id="sidebar">
+        <Link to="/">
+          {" "}
+          <h3>Dashboard</h3>{" "}
+        </Link>
+        <hr />
+        <Link to="/about">
+          {" "}
+          <h3>About</h3>{" "}
+        </Link>
+        <hr />
+        <Link to="/addRecipe">
+          {" "}
+          <h3>Add Recipe</h3>{" "}
+        </Link>
+        <hr />
+      </div>
+    </>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
