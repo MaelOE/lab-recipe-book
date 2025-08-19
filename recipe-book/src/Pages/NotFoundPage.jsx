b5559eb2aac748bdf7776e6cdb3129400351a48b
dillandatanerd <dillanpatelnz@gmail.com>
2025-08-19 21:15:54 +0200

    add a error page

--- recipe-book/src/Pages/NotFoundPage.jsx
+++ recipe-book/src/Pages/NotFoundPage.jsx
@@ -1,6 +1,21 @@
+import Sidebar from "../components/Sidebar";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+
 function NotFoundPage() {
   return (
-    <div>NotFoundPage</div>
-  )
+    <>
+      <Navbar />
+      <div>
+        <div id="mainSection">
+          <Sidebar />
+          <h1>Error 404: We could not find this page but don't fret. Not all those that wander are lost. </h1>
+        </div>
+      </div>
+
+      <Footer />
+    </>
+  );
 }
 export default NotFoundPage
